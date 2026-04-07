d756f5982935c2f4b764bcb66aa6a29130f014b8
Rubaba <nusheenrubaba@gmail.com>
2026-04-07 23:33:08 +0600

    Sprint 1: Developer 2 tasks

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
-Route::get('/', [UserController::class, 'index'])->name('home');
-Route::get('/login', [UserController::class, 'login'])->name('login');
-Route::post('/login', [UserController::class, 'authenticate'])->name('login.authenticate');
-Route::get('/register', [UserController::class, 'register'])->name('register');
-Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
